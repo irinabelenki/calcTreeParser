8dc1eb6a51bc110d5ca192cd4e06d683a0e296d2
Joseph Hindin <hindin@gmail.com>
2015-07-04 05:43:07 +0300

    Cosmetics - reformat

--- src/calcTreeParser/Parser.java
+++ src/calcTreeParser/Parser.java
@@ -8,81 +8,75 @@ public class Parser {
 	private List<String> tokensList = new ArrayList<String>();
 	private ListIterator<String> tokensListIt = null;
 	private char DELIMITER = ' ';
-		
+
 	public Parser(String line) {
 		parse(line.trim());
 	}
-	
-	enum TOKEN_TYPE { OPERATION, DIGIT, UNKNOWN };
-	
+
+	enum TOKEN_TYPE {
+		OPERATION, DIGIT, UNKNOWN
+	};
+
 	private void parse(String line) {
 		char[] array = line.toCharArray();
-		
+
 		char currChar;
 		StringBuilder tokenBuilder = null;
 		TOKEN_TYPE tokenType = TOKEN_TYPE.UNKNOWN;
-		
+
 		for (char ch : array) {
-		    currChar = ch;
-		    if(isOperation(currChar)) {
-		    	if(tokenBuilder != null) {
-		    		tokensList.add(tokenBuilder.toString());
-		    	}
-		    	tokenBuilder = new StringBuilder();
-		    	tokenBuilder.append(ch);
-		    	tokenType = TOKEN_TYPE.OPERATION;
-		    	tokensList.add(tokenBuilder.toString());
-		    	tokenBuilder = null;
-		    }
-		    else if (Character.isDigit(currChar)) {
-		    	if(tokenBuilder != null) {
-		    		if (tokenType == TOKEN_TYPE.DIGIT) {
-		    			tokenBuilder.append(ch);
-		    		}
-		    		else {
-		    			tokensList.add(tokenBuilder.toString());
-				    	tokenBuilder = null;
-		    		}
-		    	}
-		    	else {
-		    		tokenBuilder = new StringBuilder();
-		    		tokenBuilder.append(ch);
-		    		tokenType = TOKEN_TYPE.DIGIT;
-		    	}
-		    }
-		    else if (currChar == DELIMITER) {
-		    	if (tokenBuilder != null) {
-		    		tokensList.add(tokenBuilder.toString());
-		    		tokenBuilder = null;
-		    	}
-		    }
-		    else {
-		    	if(tokenBuilder != null) {
-		    		if (tokenType == TOKEN_TYPE.UNKNOWN) {
-		    			tokenBuilder.append(ch);
-		    		}
-		    		else {
-		    			tokensList.add(tokenBuilder.toString());
-				    	tokenBuilder = null;
-		    		}
-		    	}
-		    	else {
-		    		tokenBuilder = new StringBuilder();
-		    		tokenBuilder.append(ch);
-		    		tokenType = TOKEN_TYPE.UNKNOWN;
-		    	}
-		    }
-		}//for
-		if(tokenBuilder != null) {
-    		tokensList.add(tokenBuilder.toString());
-    	}
+			currChar = ch;
+			if (isOperation(currChar)) {
+				if (tokenBuilder != null) {
+					tokensList.add(tokenBuilder.toString());
+				}
+				tokenBuilder = new StringBuilder();
+				tokenBuilder.append(ch);
+				tokenType = TOKEN_TYPE.OPERATION;
+				tokensList.add(tokenBuilder.toString());
+				tokenBuilder = null;
+			} else if (Character.isDigit(currChar)) {
+				if (tokenBuilder != null) {
+					if (tokenType == TOKEN_TYPE.DIGIT) {
+						tokenBuilder.append(ch);
+					} else {
+						tokensList.add(tokenBuilder.toString());
+						tokenBuilder = null;
+					}
+				} else {
+					tokenBuilder = new StringBuilder();
+					tokenBuilder.append(ch);
+					tokenType = TOKEN_TYPE.DIGIT;
+				}
+			} else if (currChar == DELIMITER) {
+				if (tokenBuilder != null) {
+					tokensList.add(tokenBuilder.toString());
+					tokenBuilder = null;
+				}
+			} else {
+				if (tokenBuilder != null) {
+					if (tokenType == TOKEN_TYPE.UNKNOWN) {
+						tokenBuilder.append(ch);
+					} else {
+						tokensList.add(tokenBuilder.toString());
+						tokenBuilder = null;
+					}
+				} else {
+					tokenBuilder = new StringBuilder();
+					tokenBuilder.append(ch);
+					tokenType = TOKEN_TYPE.UNKNOWN;
+				}
+			}
+		}// for
+		if (tokenBuilder != null) {
+			tokensList.add(tokenBuilder.toString());
+		}
 		tokensListIt = tokensList.listIterator();
 	}
-	
+
 	private boolean isOperation(char ch) {
-		if (ch == '+' || ch == '-' || 
-			ch == '*' || ch == '/' ||
-			ch == '(' || ch == ')') {
+		if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '('
+				|| ch == ')') {
 			return true;
 		}
 		return false;
@@ -91,9 +85,9 @@ public class Parser {
 	public boolean hasMoreTokens() {
 		return tokensListIt.hasNext();
 	}
-	
+
 	public String nextToken() {
 		return tokensListIt.next();
 	}
-	
+
 }
